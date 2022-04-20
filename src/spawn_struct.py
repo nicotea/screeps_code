dbd506931fdf210642347e3b01b9dd11b7b4f8d3
Raphael Heo-Hsien-Kai <raphaelhhk@gmail.com>
2022-04-21 09:40:48 +1000

    Added spawn hauler with allocation of role

--- src/spawn_struct.py
+++ src/spawn_struct.py
@@ -2,17 +2,26 @@ from defs import *
 
 def run_spawn(spawn):
     
-    # nr_harvester = _.filter(Game.creeps, Game.creeps.memory.role == 'harvester')
-    # console.log(nr_harvester)
-    # nr_hauler =
-    # If nr_harvester <=  2 * nr_hauler, build harvester
+    nr_harvester = _.filter(Game.creeps, lambda creep: creep.memory.role == 'harvester')
+    console.log('number of harvester:' + len(nr_harvester))
 
-    # Spawn harvester
-    creep_name = 'Harvester' +  Game.time
-    spawn.spawnCreep([WORK, WORK, MOVE], creep_name, { 'memory': {'role': 'harvester'}})
-    # console.log('Spawn:' + creep_name)
+    nr_hauler = _.filter(Game.creeps, lambda creep: creep.memory.role == 'hauler')
+    console.log('number of hauler:' + len(nr_hauler))
+
+    console.log(nr_harvester <= nr_hauler)
+
+    # If nr_harvester <=  2 * nr_hauler, build harvester
+    if len(nr_harvester) <= len(nr_hauler / 2) or len(nr_harvester) == 0 :
+        # Spawn harvester
+        creep_name = 'Harvester' +  Game.time
+        spawn.spawnCreep([WORK, WORK, MOVE], creep_name, { 'memory': {'role': 'harvester'}})    
+
 
     # Otherwise, build hauler  
-    # creep_name = 'Hauler' +  Game.time
-    # spawn.spawnCreep([CARRY, CARRY, MOVE, MOVE], creep_name, { 'memory': {'role': 'hauler'}})
+    else:
+        creep_name = 'Hauler' +  Game.time
+        spawn.spawnCreep([CARRY, CARRY, MOVE, MOVE], creep_name, { 'memory': {'role': 'hauler'}})
+
+    # for screeps
+    # Game.spawns['Spawn1'].spawnCreep([CARRY, CARRY, MOVE, MOVE], 'Hauler' +  Game.time, { 'memory': {'role': 'hauler'}})
 
